431889a5e1e5ca134b6f456213d930e4fb0d08c1
Piotr Macioł <pmaciol@agh.edu.pl>
2020-02-11 23:37:24 +0100

    Passing comparer to premise

--- universal_reasoner/rule.h
+++ universal_reasoner/rule.h
@@ -4,6 +4,7 @@
 #include "executableWithCost.h"
 #include "fact.h"
 #include <memory>
+#include <functional>
 
 namespace ureasoner
 {
@@ -13,22 +14,28 @@ namespace ureasoner
 		virtual bool Evaluate() = 0;
 	};
 
-	template<typename T>
+	template<typename T, template<typename> typename FACT_PROVIDER = FactWithValue>
 	class PremiseWithType : public Premise
 	{
 	public:
-		typedef  FactWithValue<T> LocalFact;
-		PremiseWithType(std::shared_ptr<const LocalFact> compareTo, /*const bool (*comparer)(const std::shared_ptr<const LocalFact>, */const LocalFact& comp2)
-			: compareTo(compareTo), factToCheck(comp2)/*, comparer(comparer) */{};
+		template<typename TF>
+		using FactProvider = FACT_PROVIDER<TF>;
+		typedef FactProvider<T> LocalFact;
+		using FactValue = T;
+		PremiseWithType(std::shared_ptr<const LocalFact> compareTo, 
+						const LocalFact& comp2,
+			bool (*comparer)(const FactValue&, const FactValue&) /*= &std::equal_to<FactValue>::operator()*/)
+			: compareTo(compareTo), factToCheck(comp2), comparer(comparer) {};
 		virtual bool Evaluate()
 		{
-			return (factToCheck.isEqual(compareTo));
-			//comparer(factToCheck, compareTo);
+			//return (factToCheck.isEqual(compareTo));
+			return comparer(factToCheck.GetValue(), compareTo->GetValue());
 		}
 	protected:
 		/*const */std::shared_ptr<const LocalFact> compareTo;
 		/*const*/ LocalFact factToCheck;
-//		const bool (*comparer)(const std::shared_ptr<const Fact>, const std::shared_ptr < const Fact>);
+		//const std::function<bool(FactValue, FactValue)> comparer;
+		bool (*comparer)(const FactValue&, const FactValue&);
 	};
 
 	template<typename COST>
--- universal_reasoner_test/test.cpp
+++ universal_reasoner_test/test.cpp
@@ -3,10 +3,18 @@
 #include "../universal_reasoner/rule.h"
 
 using namespace ureasoner;
-TEST(BasicFacts, MakingFact) 
+TEST(BasicPremises, MakingPremise) 
 {
 	auto a1 = std::make_shared<FactWithValue<double>>(2.0);
 	FactWithValue<double> a2(2.0);
-	PremiseWithType<double> p(a1, a2);
+	PremiseWithType<double> p(a1, a2, [](const double& x, const double& y)->bool { return x == y; });
 	EXPECT_TRUE(p.Evaluate());
+}
+
+TEST(BasicPremises, MakingPremiseWithVirtual)
+{
+	auto a1 = std::make_shared<FactWithValue<double>>(2.0);
+	FactWithValue<double> a2(2.0);
+	std::shared_ptr<Premise> p = std::make_shared<PremiseWithType<double>>(a1, a2, [](const double& x, const double& y)->bool { return x == y; });
+	EXPECT_TRUE(p->Evaluate());
 }
